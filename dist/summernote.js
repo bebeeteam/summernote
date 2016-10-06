db3a7a9db605769725a80fb18a7a4b448bb5bf76
Jónatan Núñez <jnunez@bebee.com>
2016-10-06 17:37:27 +0200

    grunt dist

--- dist/summernote.js
+++ dist/summernote.js
@@ -6,7 +6,7 @@
  * Copyright 2013-2016 Alan Hong. and other contributors
  * summernote may be freely distributed under the MIT license./
  *
- * Date: 2016-10-05T16:10Z
+ * Date: 2016-10-06T15:36Z
  */
 (function (factory) {
   /* global define */
